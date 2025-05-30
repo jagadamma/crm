be3a6168a5ee89e68bb86559cb38b841889a7250
Soni verma <sonimac@MacBookAir.lan>
2025-05-30 13:34:18 +0530

    build fix added 8080

--- backend/app.js
+++ backend/app.js
@@ -91,5 +91,5 @@ app.get('*', (req, res) => {
 });
 
 // Dynamic port for GCP or local
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));

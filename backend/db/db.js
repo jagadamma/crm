bf33465727281ebb2dca4e1788d6e53bc7022592
Soni verma <sonimac@MacBookAir.lan>
2025-06-02 16:19:31 +0530

    cloudbuild change v3

--- backend/db/db.js
+++ backend/db/db.js
@@ -38,18 +38,39 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize({
-    dialect: 'mysql',
-    host: process.env.DB_HOST || '127.0.0.1',  // Use local MySQL if not set
-    port: process.env.DB_PORT || 3306,  // Default to 3306 (or the port you specified in .env)
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    dialectOptions: process.env.INSTANCE_CONNECTION_NAME ? {
-        socketPath: `/cloudsql/${process.env.INSTANCE_CONNECTION_NAME}`,  // For Cloud SQL if you're using it
-        connectTimeout: 10000,
-    } : {},
-});
+// const sequelize = new Sequelize({
+//     dialect: 'mysql',
+//     host: process.env.DB_HOST || '127.0.0.1',  // Use local MySQL if not set
+//     port: process.env.DB_PORT || 3306,  // Default to 3306 (or the port you specified in .env)
+//     username: process.env.DB_USER,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_NAME,
+//     dialectOptions: process.env.INSTANCE_CONNECTION_NAME ? {
+//         socketPath: `/cloudsql/${process.env.INSTANCE_CONNECTION_NAME}`,  // For Cloud SQL if you're using it
+//         connectTimeout: 10000,
+//     } : {},
+// });
+let sequelize;
+
+if (process.env.INSTANCE_CONNECTION_NAME) {
+
+    console.log("------- INSTANCE_CONNECTION_NAME ------", INSTANCE_CONNECTION_NAME)
+    // On GCP, use socketPath (Cloud SQL)
+    sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+        dialect: 'mysql',
+        dialectOptions: {
+            socketPath: `/cloudsql/${process.env.INSTANCE_CONNECTION_NAME}`,
+            connectTimeout: 10000,
+        },
+    });
+} else {
+    // Local development
+    sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+        host: process.env.DB_HOST || '127.0.0.1',
+        port: process.env.DB_PORT || 3306,
+        dialect: 'mysql',
+    });
+}
 
 module.exports = sequelize;
 
